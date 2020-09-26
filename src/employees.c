e94995c700b2aae39482987ec82ea386397026e7
nickkhall <nick.hall@bluelogicteam.com>
2020-09-26 15:38:11 -0500

    fixed include directives for shared libs

--- src/employees.c
+++ src/employees.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <libpq-fe.h>
-#include "serialize.h"
-#include "db.h"
+#include <serialize.h>
+#include <db.h>
 
 #include "../include/employees.h"
 #include "../include/utils.h"
